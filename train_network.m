a320fa6608a9cdb411979c54c1ea21873f8a45b1
ProperChaos <steven@properchaos.nl>
2014-02-17 17:20:28 +0100

    Deleted deprecated code.

--- train_network.m
+++
@@ -1,7 +0,0 @@
-function net = train_network(image_features, class_distributions)
-%     net = patternnet(50);
-%     [net, tr] = train(net, image_features', class_distributions');
-%     nntraintool;
-
-      net = TreeBagger(50, image_features, class_distributions, 'Method', 'regression');
-end
--- validate_model.m
+++
@@ -1,8 +0,0 @@
-function rmse = validate_model(net, features, solutions)
-    testY = net.predict(features);
-    
-    delta = solutions(:, 1) - testY;
-    delta = delta .^ 2;
-    
-    rmse = sqrt(mean(delta(:)));
-end
