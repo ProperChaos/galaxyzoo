1e219b9b45653c10140d38fda78cd9c0f5b9ad9e
ProperChaos <steven@properchaos.nl>
2014-02-19 22:01:22 +0100

    Tested using all samples. RMSE=0.14683

--- scikit/train.py
+++ scikit/train.py
@@ -4,11 +4,11 @@ from sklearn import cross_validation
 from numpy import array
 
 def main():
-	train = array(csv_io.read_data("features_test.csv"))
-	target = array(csv_io.read_data("solutions_test.csv"))
+	train = array(csv_io.read_data("features.csv"))
+	target = array(csv_io.read_data("solutions.csv"))
 
-	rf = RandomForestRegressor(n_estimators=10, min_samples_split=2, n_jobs=-1)
-	scores = cross_validation.cross_val_score(rf, train, target, cv=2, scoring='mean_squared_error');
+	rf = RandomForestRegressor(n_estimators=50, min_samples_split=2, n_jobs=-1)
+	scores = cross_validation.cross_val_score(rf, train, target, cv=5, scoring='mean_squared_error');
 	scores = -scores;
 	rmse = numpy.mean(numpy.sqrt(scores));
 
